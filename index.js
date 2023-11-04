88a9998fe43070cc93dbb60f810cdf8deaa8e1cc
jorgemingoia <mingoia@gmail.com>
2023-11-04 17:34:59 -0300

    Update index.js update for v2 1734

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node Azure changed!\n'
+  const msg = 'Hello Node Azure changed! v2 new release :)\n'
   res.end(msg);
 });
 
