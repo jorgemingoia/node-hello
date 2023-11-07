599f3f6d2966daf63d4071ce2c38a796da2e7718
jorgemingoia <mingoia@gmail.com>
2023-11-07 18:02:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node Azure changed! v2 new release :)\n'
+  const msg = 'Hello Node Azure changed! v2 new release :) add badge\n'
   res.end(msg);
 });
 
